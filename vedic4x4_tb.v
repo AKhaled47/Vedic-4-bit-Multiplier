f563a8bd0dcefdd9911c9dbaf7be4dd7e2a0c8f5
AKhaled47 <125220041+AKhaled47@users.noreply.github.com>
2023-07-19 13:49:39 +0300

    Update vedic4x4_tb.v

--- vedic4x4_tb.v
+++ vedic4x4_tb.v
@@ -1,16 +1,21 @@
 module vedic4x4_tb;
-
 reg [3:0] a,b;
 wire [8:0] s;
 
 vedic4x4 u1 (a,b,s);
-integer i,j;
+integer i,j,ref_mul;
 initial begin
     $monitor("A= %d , B= %d , S=%d",a,b,s);
     for(i=0;i<16;i=i+1) begin
         for (j=0;j<16;j=j+1) begin
             a=i;
             b=j;
+            ref_mul=a*b;
+            #1;
+            if(ref_mul==s)
+            $display("Pass");
+            else
+            $display("Failed : a = %d, b = %d, s = %d is not right, the right is %d",a,b,s,ref_mul);
             #1;
         end
     end
